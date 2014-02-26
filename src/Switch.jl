51406a6b099d32086f54efddde284ae03e4bd7d2
Daniel Jones <dcjones@cs.washington.edu>
2014-02-25 16:32:36 -0800

    Simplify.

--- src/Switch.jl
+++ src/Switch.jl
@@ -8,12 +8,18 @@ macro switch(value, body)
     cases = {}
     bodies = {}
 
+    endlabel = gensym("switchend")
+
     for arg in body.args
         if isa(arg, Expr) && arg.head == :macrocall && arg.args[1] == symbol("@case")
             push!(cases, arg.args[2])
             push!(bodies, {})
         elseif !isempty(bodies)
-            push!(bodies[end], arg)
+            if isa(arg, Expr) && arg.head == :break
+                push!(bodies[end], Expr(:symbolicgoto, endlabel))
+            else
+                push!(bodies[end], esc(arg))
+            end
         end
     end
 
@@ -22,8 +28,8 @@ macro switch(value, body)
     dispatch = {}
     for (case, label) in zip(cases, labels)
         ex = quote
-            if __switch_value == $(case)
-                @goto $(label)
+            if switch_value == $(case)
+                $(Expr(:symbolicgoto, label))
             end
         end
         push!(dispatch, ex)
@@ -31,16 +37,17 @@ macro switch(value, body)
 
     labeledbody = {}
     for (body, label) in zip(bodies, labels)
-        push!(labeledbody, :(@label $(label)))
+        push!(labeledbody, Expr(:symboliclabel, label))
         push!(labeledbody, body...)
     end
 
     quote
-        while true
-            local switch_value = $(value)
+        begin
+            local switch_value = $(esc(value))
             $(dispatch...)
+            $(Expr(:symbolicgoto, endlabel))
             $(labeledbody...)
-            break
+            $(Expr(:symboliclabel, endlabel))
         end
     end
 end
